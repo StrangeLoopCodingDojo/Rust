33f3dbb5672796e7198ca8f8f2891659f705b577
Geoffroy Couprie <geo.couprie@gmail.com>
2015-09-24 13:32:03 -0500

    moved tests to separate test file

---
+++ roman/tests/test.rs
@@ -0,0 +1,29 @@
+  extern crate roman;
+  use roman::*;
+
+  #[test]
+  fn iv_plus_i_eq_v() {
+    assert_eq!(&add("IV", "I"), "V");
+  }
+
+  #[test]
+  fn iii_plus_ii_eq_v() {
+    assert_eq!(&add("III", "II"), "V");
+  }
+
+  #[test]
+  fn i_plus_i_eq_ii() {
+    let foo = add("I", "I");
+    assert_eq!(&foo, "II");
+  }
+
+  #[test]
+  fn i_plus_ii_eq_iii() {
+    let foo = add("I", "II");
+    assert_eq!(&foo, "III");
+  }
+
+  #[test]
+  fn ii_plus_ii_eq_iv() {
+    assert_eq!(&add("II", "II"), "IV");
+  }
