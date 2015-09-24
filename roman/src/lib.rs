a14c467df626586781af4fadcd4044a8f85f112e
Geoffroy Couprie <geo.couprie@gmail.com>
2015-09-24 12:47:03 -0500

    we have a working test

--- roman/src/lib.rs
+++ roman/src/lib.rs
@@ -1,6 +1,9 @@
-
+fn add(x:String, y:String) -> String {
+  x + &y
+}
 
 #[test]
 fn it_works() {
-  assert!(false);
+  let foo = add("I".to_string(), "I".to_string());
+  assert_eq!(&foo, "II");
 }
