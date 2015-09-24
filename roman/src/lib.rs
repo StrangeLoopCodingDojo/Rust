80a433a42fab4516f3133f4337d878f10a2be35e
Geoffroy Couprie <geo.couprie@gmail.com>
2015-09-24 12:51:49 -0500

    cleaning up function params

--- roman/src/lib.rs
+++ roman/src/lib.rs
@@ -1,9 +1,9 @@
-fn add(x:String, y:String) -> String {
-  x + &y
+fn add(x:&str, y:&str) -> String {
+  String::new() + x + y 
 }
 
 #[test]
 fn it_works() {
-  let foo = add("I".to_string(), "I".to_string());
+  let foo = add("I", "I");
   assert_eq!(&foo, "II");
 }
